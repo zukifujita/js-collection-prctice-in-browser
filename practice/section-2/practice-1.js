aefcaf2e2cf3d298b6a295f630d517dc3e1f7de0
Mizuki Fujita <mizuki.fujita@oocl.com>
2019-09-15 23:39:29 +0900

    Section-2

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,5 +1,25 @@
 'use strict';
 
 function countSameElements(collection) {
-  return 'Implement the practice require, and begin changing code in this row';
+  var key = [];
+  var result = [];
+  var count;
+
+  for (var i = 0; i < collection.length; i++) {
+    count = 0;
+    key = collection[i];
+    for (var j = i; j < collection.length; j++) {
+      if(collection[i] == collection[j]) {
+        count++;
+      } else {
+        result.push({key, count});
+        break;
+      }
+    }
+    i = j - 1;
+    if(i + 1 == collection.length){
+      result.push({key, count});
+    }
+  }
+  return result;
 }
--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,31 @@
 'use strict';
 
 function countSameElements(collection) {
-  return 'Implement the practice require, and begin changing code in this row';
+  var key = [];
+  var result = [];
+  var count;
+
+  for (var i = 0; i < collection.length; i++) {
+    count = 0;
+    key = collection[i];
+    if (collection[i].match([A-Za-z])) {
+      key = collection[i].replace([A-Za-z], "");
+    }
+    for (var j = i; j < collection.length; j++) {
+      if(collection[i] == collection[j]) {
+        if(collection[j].match([0-9])) {
+          
+        }
+        count++;
+      } else {
+        result.push({key, count});
+        break;
+      }
+    }
+    i = j - 1;
+    if(i + 1 == collection.length){
+      result.push({key, count});
+    }
+  }
+  return result;
 }
