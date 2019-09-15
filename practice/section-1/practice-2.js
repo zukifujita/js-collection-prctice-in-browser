1bda3ef7003c4406f9d8d76bb95963c24b40bcd3
Mizuki Fujita <mizuki.fujita@oocl.com>
2019-09-15 23:38:45 +0900

    Section-1

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,11 +1,11 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  var result = [[]];
+  var result = [];
   for (var i = 0; i < collectionA.length; i++) {
-    for (var j = 0; j < collectionB.length; j++) {
-      if(collectionA[i] == collectionB[j]) {
-        result.push(collectionA[i]);
+    for (var j = 0; j < collectionB[0].length; j++) {
+      if(collectionA[i] == collectionB[0][j]) {
+        result.push(collectionB[0][j]);
       }
     }
   }
