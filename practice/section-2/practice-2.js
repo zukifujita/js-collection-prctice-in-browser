94301371b427eec6d3d63a0c1258ab64a3d0525b
Mizuki Fujita <mizuki.fujita@oocl.com>
2019-09-16 00:12:30 +0900

    Section-2

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -3,20 +3,25 @@
 function countSameElements(collection) {
   var key = [];
   var result = [];
+  var str;
   var count;
 
   for (var i = 0; i < collection.length; i++) {
     count = 0;
     key = collection[i];
-    if (collection[i].match([A-Za-z])) {
-      key = collection[i].replace([A-Za-z], "");
+    if (collection[i].match(/[A-Za-z]\W[0-9]/g)) {
+      str = collection[j].split(/\W/g);
+      key = str[0];
     }
     for (var j = i; j < collection.length; j++) {
       if(collection[i] == collection[j]) {
-        if(collection[j].match([0-9])) {
-          
+        if(collection[j].match(/[0-9]/g)) {
+          str = collection[j].split(/\W/g);
+          var num = str[1];
+          count += parseInt(num);
+        } else {
+          count++;
         }
-        count++;
       } else {
         result.push({key, count});
         break;
--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,5 +1,36 @@
 'use strict';
 
 function countSameElements(collection) {
-  return 'Implement the practice require, and begin changing code in this row';
+  var name = [];
+  var result = [];
+  var str;
+  var count;
+
+  for (var i = 0; i < collection.length; i++) {
+    count = 0;
+    name = collection[i];
+    if (collection[i].match(/[A-Za-z]\W[0-9]/g)) {
+      str = collection[j].split(/\W/g);
+      name = str[0];
+    }
+    for (var j = i; j < collection.length; j++) {
+      if(collection[i] == collection[j]) {
+        if(collection[j].match(/[0-9]/g)) {
+          str = collection[j].split(/\W/g);
+          var num = str[1];
+          count += parseInt(num);
+        } else {
+          count++;
+        }
+      } else {
+        result.push({name, count});
+        break;
+      }
+    }
+    i = j - 1;
+    if(i + 1 == collection.length){
+      result.push({name, count});
+    }
+  }
+  return result;
 }
